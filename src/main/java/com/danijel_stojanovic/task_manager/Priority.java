4cd250f77809de81f702ab0c210e698147872e5b
DanijelStojanovic777 <danijel.priv@gmail.com>
2025-07-23 23:02:34 +0200

    Stage 1

---
+++ src/main/java/com/danijel_stojanovic/task_manager/Priority.java
@@ -0,0 +1,9 @@
+package com.danijel_stojanovic.task_manager;
+
+public enum Priority {
+
+    LOW,
+    MEDIUM,
+    HIGH
+
+}
---
+++ src/main/java/com/danijel_stojanovic/task_manager/Task.java
@@ -0,0 +1,87 @@
+package com.danijel_stojanovic.task_manager;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Data
+//@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Priority priority;
+    private Status status;
+    private LocalDate dueDate;
+    private User assignedUser;
+
+    public Task(Long id, String title, String description, Priority priority, Status status, LocalDate dueDate, User assignedUser) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+        this.dueDate = dueDate;
+        this.assignedUser = assignedUser;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public User getAssignedUser() {
+        return assignedUser;
+    }
+
+    public void setAssignedUser(User assignedUser) {
+        this.assignedUser = assignedUser;
+    }
+}
